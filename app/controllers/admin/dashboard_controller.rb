c57a9f23e2fec9570a45ffd84b00a934d1dd8c87
xli52 <lxmichael52@gmail.com>
2022-06-30 14:52:30 -0700

    Added basic http auth feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
+  
   def show
     @product_count = Product.all.count
     @category_count = Category.all.count
